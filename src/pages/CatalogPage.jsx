301525c7ee98286cda563876db0d28430424477f
Andriy Nevrov <your-email@example.com>
2026-05-09 16:15:49 +0300

    Lab 13 React

--- src/pages/CatalogPage.jsx
+++ src/pages/CatalogPage.jsx
@@ -1,21 +1,61 @@
-import { Link } from "react-router-dom";
-
-const books = [
-  { id: 1, title: "Кобзар" },
-  { id: 2, title: "Лісова пісня" },
-  { id: 3, title: "Захар Беркут" }
-];
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function CatalogPage() {
+  const [books, setBooks] = useState([]);
+  const [categories, setCategories] = useState([]);
+
+  // Завантаження книг
+  const fetchBooks = async (category = "") => {
+    let url = "http://127.0.0.1:8000/api/books";
+
+    if (category) {
+      url += `?category=${category}`;
+    }
+
+    const response = await axios.get(url);
+    setBooks(response.data);
+  };
+
+  // Завантаження категорій
+  const fetchCategories = async () => {
+    const response = await axios.get(
+      "http://127.0.0.1:8000/api/categories"
+    );
+
+    setCategories(response.data);
+  };
+
+  useEffect(() => {
+    fetchBooks();
+    fetchCategories();
+  }, []);
+
   return (
     <div className="page">
       <h2>📚 Каталог книг</h2>
 
+      {/* Кнопки категорій */}
+      <div>
+        <button onClick={() => fetchBooks()}>
+          Всі
+        </button>
+
+        {categories.map((cat, index) => (
+          <button
+            key={index}
+            onClick={() => fetchBooks(cat)}
+          >
+            {cat}
+          </button>
+        ))}
+      </div>
+
+      {/* Книги */}
       {books.map((book) => (
         <div key={book.id} className="book-item">
-          <Link to={`/book/${book.id}`}>
-            {book.title}
-          </Link>
+          <h3>{book.title}</h3>
+          <p>{book.category}</p>
         </div>
       ))}
     </div>
