6c17c4c7e4de8d54b9bcf7c68b61161c7e061b5c
Andriy Nevrov <your-email@example.com>
2026-05-12 17:09:11 +0300

    Lab 14 React

--- src/pages/CatalogPage.jsx
+++ src/pages/CatalogPage.jsx
@@ -1,63 +1,42 @@
 import { useEffect, useState } from "react";
-import axios from "axios";
 
 function CatalogPage() {
+
   const [books, setBooks] = useState([]);
-  const [categories, setCategories] = useState([]);
-
-  // Завантаження книг
-  const fetchBooks = async (category = "") => {
-    let url = "http://127.0.0.1:8000/api/books";
-
-    if (category) {
-      url += `?category=${category}`;
-    }
-
-    const response = await axios.get(url);
-    setBooks(response.data);
-  };
-
-  // Завантаження категорій
-  const fetchCategories = async () => {
-    const response = await axios.get(
-      "http://127.0.0.1:8000/api/categories"
-    );
-
-    setCategories(response.data);
-  };
 
   useEffect(() => {
-    fetchBooks();
-    fetchCategories();
+
+    fetch("http://127.0.0.1:8000/api/books")
+      .then((response) => response.json())
+      .then((data) => {
+        setBooks(data);
+      });
+
   }, []);
 
   return (
-    <div className="page">
-      <h2>📚 Каталог книг</h2>
+    <div className="catalog">
 
-      {/* Кнопки категорій */}
-      <div>
-        <button onClick={() => fetchBooks()}>
-          Всі
-        </button>
+      <h1>Каталог книг</h1>
 
-        {categories.map((cat, index) => (
-          <button
-            key={index}
-            onClick={() => fetchBooks(cat)}
-          >
-            {cat}
-          </button>
+      <div className="books-list">
+
+        {books.map((book) => (
+
+          <div className="book-card" key={book.id}>
+
+            <h2>{book.title}</h2>
+
+            <p>Автор: {book.author}</p>
+
+            <p>Ціна: {book.price} грн</p>
+
+          </div>
+
         ))}
+
       </div>
 
-      {/* Книги */}
-      {books.map((book) => (
-        <div key={book.id} className="book-item">
-          <h3>{book.title}</h3>
-          <p>{book.category}</p>
-        </div>
-      ))}
     </div>
   );
 }
